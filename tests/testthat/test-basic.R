3b611d7e6b935380e039c95416b184c2e11d8020
hafen <rhafen@gmail.com>
2019-04-17 20:23:49 -0700

    Add configure to tests

--- tests/testthat/test-basic.R
+++ tests/testthat/test-basic.R
@@ -1,5 +1,11 @@
 context("test-basic")
 
+test_that("configure", {
+  ic <- suppressMessages(is_configured())
+  if (!ic)
+    kitools_configure()
+})
+
 test_that("import kitools", {
   kitools <- reticulate::import("kitools")
 })
